00dc46a89a0507b7939061f50da4237c23b14853
Prateek14b <prateek14b@gmail.com>
2024-12-23 21:14:30 -0500

    convertedtofunction
    - retained the logic, while converting the code into a function.

--- approximate_pi.py
+++ approximate_pi.py
@@ -2,15 +2,15 @@
 # Emails    : pbasavaraj@umass.edu, tsoni@umass.edu
 # Spire IDs : 34743797, 34558092
 
-n=int(input('Enter a number: '))
-sum=0
-i=1 #looping variable
+def piapproximate(n):
+    sum=0
+    i=1 #looping variable
 
-while i<=n:
-    sum=sum+(1/(i**2))
-    i=i+1
+    while i<=n:
+        sum=sum+(1/(i**2))
+        i=i+1
 
-print(f'sum = {sum}')
-approxval=((6*sum)**0.5)
-print(f'approximate value of pi is: {approxval}')
+    print(f'sum = {sum}')
+    approxval=((6*sum)**0.5)
+    print(f'approximate value of pi is: {approxval}')
 
