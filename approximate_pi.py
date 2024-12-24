bf22c6f299c19139e7dcdd41d9bdeb356d8ea67b
Prateek14b <50728753+Prateek14b@users.noreply.github.com>
2024-12-23 21:01:51 -0500

    Add files via upload

---
+++ approximate_pi.py
@@ -0,0 +1,16 @@
+# Authors   : Prateek Basavaraj, Tanvi Soni
+# Emails    : pbasavaraj@umass.edu, tsoni@umass.edu
+# Spire IDs : 34743797, 34558092
+
+n=int(input('Enter a number: '))
+sum=0
+i=1 #looping variable
+
+while i<=n:
+    sum=sum+(1/(i**2))
+    i=i+1
+
+print(f'sum = {sum}')
+approxval=((6*sum)**0.5)
+print(f'approximate value of pi is: {approxval}')
+
